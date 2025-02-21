fb4bdf2d742f4f0903e930f1162b49687919e6e3
M4riaDuda <eduardatakano2206@gmail.com>
2025-02-20 22:28:02 -0300

    Início da barra de navegação.

---
+++ pw3-somativa-2025-1/src/components/layout/NaveBar.jsx
@@ -0,0 +1,15 @@
+import style from './NaveBar.module.css'
+
+const NavBar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>CADASTRAR LIVROS</li>
+                <li>LISTAR LIVROS</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
