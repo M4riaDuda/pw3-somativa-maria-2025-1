878df97bf2c78f72b6935ab58c0a984a577c768b
M4riaDuda <eduardatakano2206@gmail.com>
2025-04-24 21:44:09 -0300

    Desenvolvimento da tela de listagem de livros

--- pw3-somativa-2025-1/src/components/BookCard.jsx
+++ pw3-somativa-2025-1/src/components/BookCard.jsx
@@ -1,11 +1,11 @@
 import Button from './Button'
 import style from './BookCard.module.css'
 
-const BookCard = ({titulo, autor, imagem}) => {
+const BookCard = ({cod_livro, nome_livro, autor_livro, imagem}) => {
     return(
         <div className={style.bookCard}>
-            <h3 className={style.title}>{titulo}</h3>
-            <p className={style.author}>{autor}</p>
+            <h3 className={style.title}>{nome_livro}</h3>
+            <p className={style.author}>{autor_livro}</p>
             <img src={imagem} alt="Capa: As Cavernas de Aço"/>
             <Button label='DETALHE' />
         </div>
